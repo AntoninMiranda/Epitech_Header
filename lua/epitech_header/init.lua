ac84f5b3dac2e9295922e70d36b6dc3ddeba30b6
AntoninMiranda <antonin.miranda@epitech.eu>
2023-11-25 14:32:54 +0100

    [~] setup run create

--- lua/epitech_header/init.lua
+++ lua/epitech_header/init.lua
@@ -3,6 +3,7 @@ local utils = require("epitech_header.epitech_header.header")
 local M = {}
 
 function M.setup(config)
+    -- utils.conf_tab.name[1] = config.name[1]
 end
 
 M.header = utils.create
