0ad81a155fd31d86896980a3eab47868e9845481
AntoninMiranda <antonin.miranda@epitech.eu>
2023-11-25 14:20:01 +0100

    [~] setup run create

--- lua/epitech_header/init.lua
+++ lua/epitech_header/init.lua
@@ -3,7 +3,7 @@ local utils = require("epitech_header.epitech_header.header")
 local M = {}
 
 function M.setup(config)
-    vim.g.hedaer_authors = config.name[1]
+    vim.g.header_authors = config.name[1]
 end
 
 M.header = utils.create
